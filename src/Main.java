94b50bfeb81b69e85168dba4b24fd426664cae6f
Dazi Dev <shadowapex308@gmail.com>
2026-07-29 19:53:06 +0100

    loops

--- src/Main.java
+++ src/Main.java
@@ -8,18 +8,56 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        // FizzBuzz
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Number: ");
-        int number = scanner.nextInt();
+            // Loops
+        // For Loops
+//        for(int  i = 0; i < 5; i++) {
+//            //System.out.println("Hello World");
+//        }
+//        // While Loops use when we dont know how many times we want to execute a statements
+//        int i = 0;
+//        while(i > 0 ) {
+//            //System.out.println("Hello World" + 1);
+//            i--;
+//        }
+        // practice exemple
+        //Scanner scanner = new Scanner(System.in);
+       // String input = "";
+       // while (!input.equals("quit")) {
+            //System.out.println("Input : ");
+            //input = scanner.next().toLowerCase();
+            //System.out.println(input);
+       // }
+        // Do while loop similar to while but get executed atleast once
+//        Scanner scanner = new Scanner(System.in);
+//        String input = "";
+//        do {
+//            System.out.print("Input: ");
+//            input = scanner.next().toLowerCase();
+//            System.out.println(input);
+//        } while (!input.equals("quit"));
 
-        if (number % 5 == 0 && number % 3 == 0)
-            System.out.println("FizzBuzz");
-        else if (number % 3 == 0)
-            System.out.println("Buzz");
-        else if (number % 5 == 0)
-            System.out.println("Fizz");
-        else
-            System.out.println(number);
+        // break and continue
+//        Scanner scanner = new Scanner(System.in);
+//        String input = "";
+//        while (!input.equals("quit")) {
+//            System.out.print("Input :" );
+//            input = scanner.next().toLowerCase();
+//            if(input.equals("pass"))
+//                continue;
+//            if(!input.equals("quit"))
+//                break;
+//            System.out.println(input);
+//        }
+
+        // For each loop in java
+        String[] fruits = {"apple", "mango", "orange"};
+
+//        for (int i = fruits.length; i > 0; i--) {
+//            System.out.println(fruits[i].toUpperCase());
+//        }
+
+        for (String fruit : fruits) {
+            System.out.println(fruit);
+        }
     }
 }
