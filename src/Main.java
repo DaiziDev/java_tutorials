dc96140176da458b8284c5f03d58b68a46154455
Dazi Dev <shadowapex308@gmail.com>
2026-07-29 17:38:43 +0100

    Math classes, numbers formating and reading input from the user

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,30 @@
 import java.awt.*;
+import java.text.NumberFormat;
 import java.util.Arrays;
 import java.util.Date;
+import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        // learn about constant add final infront of the declaration in other never to modif it
-        final float pi = 3.14f;
+        // Math class
+        int result = Math.round(1.1F);
+        // here we are casting a float to an integer
+        int result2 = (int)Math.ceil(1.1F);
+        int  result3 = (int)Math.floor(1.1F);
+        // random is for double not integer
+        int  result4 = (int) Math.round(Math.random() * 100);
 
-        // Arithmetic operations
-        // an expression is a piece of code that produces a value
-        double result = (double)10 / (double)3;
-        int x = 1;
-        //x = x + 2;
-        x += 2; //argumented operators
-        System.out.println(x);
+        // formating numbers
+       //  NumberFormat percent = NumberFormat.getPercentInstance();
+        // String results = percent.format(result);
+        String results = NumberFormat.getPercentInstance().format(result4);
 
-        //order of operations
-        int y = 10 + 3 * 2;
+        // Reading input
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Name: ");
+        String name = scanner.next().trim();
+        System.out.println("you name is  " + name);
     }
 }
