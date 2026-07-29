563d104a678bef5a09d21e833c88b8b5e475be1b
Dazi Dev <shadowapex308@gmail.com>
2026-07-29 11:43:38 +0100

    learn about strings and some of the methods like trim, uppercase ...etc

--- src/Main.java
+++ src/Main.java
@@ -5,22 +5,7 @@ import java.util.Date;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        // primitive variable type for simple values
-        byte age = 30;
-        long viewsCount = 3_123_456_789L;
-        float price = 10.99F;
-        char letter = 'A';
-        boolean isEligible = true;
-
-        // reference variabl type  for storing complex objects
-        Date now = new Date();
-
-        // major difference between primitive and reference type
-        Point point1 = new Point(1, 1);
-        Point point2 = point1;
-        System.out.println(now);
-        point1.x = 2;
-
-        System.out.println(point2);
+        String message = "   Hello     World!   ";
+        System.out.println(message.trim());
     }
 }
