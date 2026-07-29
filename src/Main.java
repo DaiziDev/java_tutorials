e45828a89db82fcbec460c967831fad23eb597b3
Dazi Dev <shadowapex308@gmail.com>
2026-07-29 12:00:54 +0100

    arrays

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,13 @@
 import java.awt.*;
+import java.util.Arrays;
 import java.util.Date;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        String message = "   Hello     World!   ";
-        System.out.println(message.trim());
+        int[][] numbers = { {1,2,3}, {4, 5, 5}};
+        System.out.println(Arrays.deepToString(numbers));
+
     }
 }
