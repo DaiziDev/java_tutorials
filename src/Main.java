ee8e4ba7842a6cd6b6ddeae15af52bcd1d008ba8
Dazi Dev <shadowapex308@gmail.com>
2026-08-02 17:20:11 +0100

    creating an object in java and using it in the main file

--- src/Main.java
+++ src/Main.java
@@ -8,42 +8,16 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-            // Mortgate calculators
-        final byte MONTHS_IN_YEAR = 12;
-        final byte PERCENT = 100;
+        System.out.println("Hello World!");
+        User u = new User();
+        u.name = "Caleb";
+        u.membership = "Gold";
 
-        Scanner scanner = new Scanner(System.in);
+        User u2 = new User();
+        u2.name = "Sally";
+        u2.membership = "Silver";
 
-        int principal;
-        while (true) {
-            System.out.println("Principal : ");
-            principal = scanner.nextInt();
-            if (principal >= 1000 && principal <= 1_000_000)
-                break;
-            System.out.println("Enter a value between 1000 and 1000000");
-        }
-
-        float monthlyInterest;
-        while (true) {
-            System.out.println("Annual Intereset Rate: ");
-            float annualInterestRate = scanner.nextFloat();
-            if (annualInterestRate >= 1 && annualInterestRate <= 30) {
-                monthlyInterest = annualInterestRate / PERCENT / MONTHS_IN_YEAR;
-                break;
-            }
-            System.out.println("Enter a value between 1 and 30");
-        }
-
-        int numberOfPayments;
-        while (true) {
-            System.out.print("Period (years): ");
-            byte years = scanner.nextByte();
-            if (years >= 1 && years <= 30) {
-                numberOfPayments = years * MONTHS_IN_YEAR;
-                break;
-            }
-            System.out.println("Enter a value between 1 and 30");
-        }
-        double mortgage = principal * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments ));
+        System.out.println(u.name);
+        System.out.println(u2.name);
     }
 }
